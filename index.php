e70996b7069984fe7af63747cb624ee0d786baff
Mario Guzman <marioguzzzman@gmail.com>
2017-09-22 13:42:56 -0300

    inicialización de repositorio

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DGC web</title>
+  <link rel="stylesheet" href="main.css">
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+  <nav class="navbar navbar-custom">
+    <!-- BARRA DE NAVEGACION -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
+          <span class="sr-only">Menú</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      <a class="navbar-brand" href="#">
+        <img alt="GDC WEB" src="img/logo-bcn-bicentenario-400x400.jpg"></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-1">
+      <ul class="nav navbar-nav navbar-right">
+        <!-- LISTA PRINCIPAL -->
+        <li><a href="index.html">Inicio</a></li>
+
+        <!-- <li><a href="preguntas_menu.php">Trivias</a></li> -->
+
+        <li class="dropdown" id="formularios">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Formularios para estadísticas<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="https://goo.gl/forms/9QoqHsrn5xe8rjaz1">Eventos</a></li>
+            <li><a href="https://goo.gl/forms/gysIiPta2igFeQLU2">Talleres</a></li>
+            <li><a href="https://goo.gl/forms/BnwRnhApxeqhbFEL2">Bibliomóvil</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="https://goo.gl/forms/veuSRsJBXMxl5L732">Eventos de Jornada múltiple</a></li>
+
+          </ul>
+        </li>
+
+        <!-- <li class="dropdown" id="acercade">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informes 2017<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="index.php">Julio-Agosto</a></li>
+          </ul>
+        </li> -->
+
+        <!-- LISTA PRINCIPAL -->
+      </ul>
+    </div>
+    <!-- BARRA DE NAVEGACION -->
+  </nav>
+
+  <div class="container">
+    <!-- CONTAINER PRINRICPAL -->
+
+    <div class="row">
+      <div class="col-md-12">
+        <header>
+          <h1>Dirección de Gestión Cultural</h1>
+          <h3>[web interna]</h3>
+        </header>
+      </div>
+    </div>
+
+    <!-- <section>
+      <div class="row">
+        <div class="col-md-12">
+          <header>
+            <h2>Formularios para Estadísticas</h2>
+          </header>
+
+          Eventos Talleres Bibliomóvil
+
+        </div>
+      </div>
+    </section> -->
+
+    <!-- CONTAINER PRINRICPAL -->
+  </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+
+</html>
